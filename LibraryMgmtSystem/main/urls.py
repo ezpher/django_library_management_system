7571984e631d80d97641f05d2f851bc0724f8cff
Hong Dah Ong <40519064+ezpher@users.noreply.github.com>
2020-07-04 06:23:51 +0800

    add templates and render view to dashboard

--- LibraryMgmtSystem/main/urls.py
+++ LibraryMgmtSystem/main/urls.py
@@ -1,4 +1,4 @@
-"""LibraryMgmtSystem URL Configuration
+"""Main App URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', views.dashboard,  name='dashboard')
 ]
--- LibraryMgmtSystem/main/views.py
+++ LibraryMgmtSystem/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import LibraryUser, BookTransaction
+from django.utils import timezone
 
 # Create your views here.
+
+def dashboard(request):
+
+    library_users = LibraryUser.objects.all()
+    book_transactions = BookTransaction.objects.filter(date_due__lt=timezone.now())
+
+    context_object = {'library_users': library_users, 'book_transactions': book_transactions}
+
+    return render(request, template_name='main/dashboard.html', context=context_object)
